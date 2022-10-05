7acc2a0c2af7324f518c1a8cd8663237c3406e79
pikachu-17 <115074433+pikachu-17@users.noreply.github.com>
2022-10-05 15:05:46 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class wonders
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        String wonders[]={"chicken itza", "christ the reedemer", "taj mahal", "great wall of china", "nachu pichu", "petre collesum"};
+        String locations[]={"MEXICO", "BRAZIL", "INDIA", "CHINA", "PERU", "JORDAN", "ITALY"};
+        
+        System.out.println("Enter the country name");
+        String c=sc.next();
+        int flag=0;
+        int len=locations.length;
+        
+        for(int i=0;i<len;i++)
+        {
+        if(c.equalsIgnoreCase(locations[i]))
+        {
+            flag=1;
+            System.out.println(locations[i]+"'s wonder is"+wonders[i]);
+            break;
+            
+        }
+        }
+        if(flag==0)
+        System.out.println("Sorry not found");
+    }
+    }
