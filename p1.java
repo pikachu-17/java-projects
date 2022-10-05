2df013a1ff89f888e4b88ebb550af2e135996d21
pikachu-17 <115074433+pikachu-17@users.noreply.github.com>
2022-10-05 15:05:22 +0530

    Create p1.java

---
+++ p1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class marksss
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n=sc.nextInt();
+        
+        int marks[]=new int[n];
+        String names[]=new String[n];
+        double avg=0;
+        int sum=0;
+        double dev=0.0d;
+        
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Enter the marks");
+            marks[i]=sc.nextInt();
+            names[i]=sc.next();
+            
+            sum=marks[i]+sum;
+        }
+        
+        avg=sum/n;
+        
+        for(int i=0;i<n;i++)
+        {
+            dev=avg-marks[i];
+            System.out.println("Deviation of"+names[i]+"is"+marks[i]);
+        }
+        System.out.println("average is"+avg);
+        
+    }
+}
+    
